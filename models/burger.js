97ba98b6f709956810d36be9e476df1d280d9f19
Lio <lionell.marquez@gmail.com>
2019-06-25 16:45:10 -0400

    Almost finished, but buttons do not work

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,18 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-    all: function(cb) {
-      orm.all("burgers_db", function(res) {
+    selectAll: function(cb) {
+      orm.selectAll("burgers_db", function(res) {
         cb(res);
       });
     },
-    create: function(cols, vals, cb) {
-      orm.create("burgers_db", cols, vals, function(res) {
+    insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers_db", cols, vals, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
-      orm.update("burger_db", objColVals, condition, function(res) {
-        cb(res);
-      });
-    },
-    delete: function(condition, cb) {
-      orm.delete("burger_db", condition, function(res) {
+    updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burger_db", objColVals, condition, function(res) {
         cb(res);
       });
     }
